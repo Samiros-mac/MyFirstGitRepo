d50ed871a56460a611783be5044690f27e580694
samir.babayev.qa.engineer.com@gmail.com <sb829009@gmail.com>
2020-06-28 20:47:34 -0400

    1st commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World samir zaur");
         System.out.println("Good evening samir hazar");
+        System.out.println("salam");
     }
 }
